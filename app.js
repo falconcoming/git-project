1fdc76d7bfe7f0c3b971304ba72effb4940a8993
codetogetplaced <codetogetplaced@gmail.com>
2021-08-07 13:06:28 +0530

    this is the first commit
    new console log done

---
+++ app.js
@@ -0,0 +1,35 @@
+console.log("hello");
+
+/* 
+intialised empty repository
+git init
+
+ls -al
+display files and folders
+contains .git
+data structures and code to track how your code is getting modified
+
+all your past progress and commit history is gone if you delete .git
+
+git status
+-> which files is present in our folder is not tracked or edited
+
+to track the file
+git add <file-name>
+
+git status
+-> shows it is tracked
+
+*/
+
+//write new code
+
+console.log("new code");
+
+/*
+
+git status
+-> shows something is untracked
+
+run git add
+
